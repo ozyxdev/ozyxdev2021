4a9c7f3d27f132a4eb551fed1c85ab42c445e43a
Omar Syx <jprzpam@gmail.com>
2021-04-28 14:22:06 +0200

    add simple dark mode using css variables

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import DarkToggle from '../components/DarkToggle'
 import Title from '../components/styles/Title'
 
 export default function Home() {
@@ -9,6 +10,7 @@ export default function Home() {
       </Head>
 
       <main>
+        <DarkToggle />
         <Title>Omar Syx</Title>
       </main>
 
