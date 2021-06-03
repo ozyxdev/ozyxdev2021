a9dc99d1c6de469140757a3bc42452f2ab42c726
Omar Syx <jprzpam@gmail.com>
2021-06-03 13:05:16 +0200

    eslint warning fix

--- src/components/shared/Nav/Navigation.js
+++ src/components/shared/Nav/Navigation.js
@@ -24,7 +24,7 @@ export default function Navigation() {
   const router = useRouter()
   useEffect(() => {
     router.events.on('routeChangeStart', () => setMenuOpen(false))
-  }, [])
+  })
 
   return (
     <NavContainer ref={constraintsRef}>
